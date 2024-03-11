bef52869f6b752aeabe34c8c68f9102b5dd7fb60
Johary <jo.rajoelisolo@etechconsulting-mg.com>
2024-03-11 15:06:14 +0300

    Delete cholera_boat

--- odoo_12/RAJOELISOLO_Johary_SI20190021/Module_cours/models/cours.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields, _
-
-class cours(models.Model):
-    _name = "cour.cour"
-
-    name = fields.Char(string="Nom de cours")
-    enseignant = fields.Char(string="Enseignant")
-    description = fields.Char(string="Déscription")
-    support = fields.Char(string="Support")
